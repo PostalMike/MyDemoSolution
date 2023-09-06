c95b7720f1111381e94276c10944d7a8a7375ed3
Mike <mdurthaler@sbcglobal.net>
2023-09-06 11:21:08 -0400

    Commented out and noted the bugged try catch added to erro.cshtml.cs
    Until we have a yaml file that includes c-sharp in the languages array, it won't be flagged.

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -23,18 +23,19 @@ namespace AngularSecurityExerciseDemo.Pages
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
 
+        /*Without a code scanning yaml file that also checks C#, this will not be picked up by code scanning*/
         //add a try catch that will trigger a vulnerability error
-        public int SomeBuggedTryCatchCode(int RequestId = RequestId)
-        {
-            try
-            {
-                //this will obviously fail, there should be an alert upon checking in this code.
-                int someInt += RequestId + "it's a sunny day."
-            }
-            catch(NullReferenceException ex)
-            {
-                Console.WriteLine(ex.ToString() + " something failed.");
-            }
-        }
+        //public int SomeBuggedTryCatchCode(int RequestId = RequestId)
+        //{
+        //    try
+        //    {
+        //        //this will obviously fail, there should be an alert upon checking in this code.
+        //        int someInt += RequestId + "it's a sunny day."
+        //    }
+        //    catch(NullReferenceException ex)
+        //    {
+        //        Console.WriteLine(ex.ToString() + " something failed.");
+        //    }
+        //}
     }
 }
